7f4eab91637dc3475ed23711164f61fdb50e3c4b
cshanto <83701801+cshanto@users.noreply.github.com>
2022-10-09 21:33:06 +0530

    Update TopTenYTvideo.java

--- TopTenYTvideo.java
+++ TopTenYTvideo.java
@@ -1,4 +1,11 @@
-// Here's the scrip to search top 10 most viewed videos on you tube and search any one amongst them in you tube
+/** Here's the scrip to search top 10 most viewed videos on you tube and search any one amongst them in you tube
+
+STEPS TO BE COVERED:
+1. Goto Google Search page and search "top 10 most viewed youtube videos of all time"
+2. Copy any of the results
+3. Navigate to YouTube and paste the copied result in the search bar and click search icon
+4. Play any of the results (here first one clicked)
+**/
 
 package mpkg;
 
@@ -19,38 +26,38 @@ public class ytSearch {
 	}
 	public static void main(String[] args) throws InterruptedException, AWTException {
 		ChromeDriver driver = new ChromeDriver();
-//		Thread.sleep(5000);
+		//Thread.sleep(5000);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
-    //go to google search page
+    		//go to google search page
 		driver.get("https://www.google.com/");
-    //search-(could be any thing)
+    		//search-(could be any thing)
 		driver.findElement(By.name("q")).sendKeys("top 10 most viewed youtube videos of all time");
 		Robot R = new Robot();
-    //press ENTER
+    		//press ENTER
 		R.keyPress(KeyEvent.VK_ENTER);
-    //release ENTER
+    		//release ENTER
 		R.keyRelease(KeyEvent.VK_ENTER);
-    //get all the text of the results page
+    		//get all the text of the results page
 		String alltext = driver.findElement(By.xpath("//div[@id='rcnt']")).getText();
-    //convert the text into an array
+    		//convert the text into an array
 		String alltextArray[]=alltext.split("\\n+");
-    //here i=3, because we need to skip unnecessary lines. So, the actual results that we need starts from line 3 of the extracted results
+    		//here i=3, because we need to skip unnecessary lines. So, the actual results that we need starts from line 3 of the extracted results
 		for(int i=3;i<=11;i++) {
-      //here i-2, because to give numbering we need to substract 2 from i to (i.e.,3 - 2 = 1). eg:1. "results line 1".....
+      			//here i-2, because to give numbering we need to substract 2 from i to (i.e.,3 - 2 = 1). eg: 1. "results line 1".....
 			System.out.println(i-2+". "+alltextArray[i]);
 		}
-    //navigate to youtube
+   	 	//navigate to youtube
 		driver.navigate().to("https://www.youtube.com/"); 
-    //send input to search bar (here alltextArray[4] = "results line 2" i.e.,i+1 = 3+1 = 4)
+    		//send input to search bar (here alltextArray[4] = "results line 2" i.e.,i+1 = 3+1 = 4)
 		driver.findElement(By.xpath("//input[@id='search']")).sendKeys(alltextArray[4]);
-    //click search icon
+    		//click search icon
 		driver.findElement(By.xpath("//button[@id='search-icon-legacy']")).click();
 		System.out.println(" ");
-    //print the search results (optional)
+    		//print the search results (optional)
 		String allvids = driver.findElement(By.xpath("//ytd-item-section-renderer[@class='style-scope ytd-section-list-renderer']")).getText();
-    //click the the first result
+    		//click the the first result
 		driver.findElement(By.xpath("//ytd-item-section-renderer[@class='style-scope ytd-section-list-renderer']/div[3]/ytd-video-renderer[1]")).click();
-//		driver.close();
+		//driver.close();
 	}
 }
